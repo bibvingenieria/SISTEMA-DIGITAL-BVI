7ba9bb94de3e462caf8b217eb736410a66b1eb80
bibvingenieria <bibv.ingenieria@urp.edu.pe>
2026-06-23 14:37:00 -0500

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1373,7 +1373,7 @@ function Admin() {
                       type="text"
                       required
                       value={publicacionEspecial.titulo}
-                      onChange={(e) => setPubilcacionEspecial({...publicacionEspecial, titulo: e.target.value})}
+                      onChange={(e) => setPublicacionEspecial({...publicacionEspecial, titulo: e.target.value})}
                       className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-orange-500 focus:border-transparent"
                       placeholder="Ej: Publicación Especial 2025"
                     />
